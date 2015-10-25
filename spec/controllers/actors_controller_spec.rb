cea06e21a3b9d010c2f2246a7a8e6e0e0b2faebd
jordi92500 <jordi92500@hotmail.fr>
2015-10-24 22:04:53 -0500

    All working

---
+++ spec/controllers/actors_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe ActorsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new_form" do
+    it "returns http success" do
+      get :new_form
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create_row" do
+    it "returns http success" do
+      get :create_row
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit_form" do
+    it "returns http success" do
+      get :edit_form
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update_row" do
+    it "returns http success" do
+      get :update_row
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #destroy" do
+    it "returns http success" do
+      get :destroy
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
